df40b7d8c6c2aab88e72cd4444c08856a436b2fb
Jesudale <daletv1852@gmail.com>
2025-01-11 11:28:24 -0600

    adicionado para actualizar

--- service-worker.js
+++ service-worker.js
@@ -3,29 +3,51 @@
  * more info on how to use sw-toolbox to custom configure your service worker.
  */
 
-
-'use strict';
-importScripts('./build/sw-toolbox.js');
-
-self.toolbox.options.cache = {
-  name: 'ionic-cache'
-};
-
-// pre-cache our key assets
-self.toolbox.precache(
-  [
-    './build/main.js',
-    './build/vendor.js',
-    './build/main.css',
-    './build/polyfills.js',
-    'index.html',
-    'manifest.json'
-  ]
-);
-
-// dynamically cache any other local assets
-self.toolbox.router.any('/*', self.toolbox.fastest);
-
-// for any other requests go to the network, cache,
-// and then only use that cached resource if your user goes offline
-self.toolbox.router.default = self.toolbox.networkFirst;
+ const CACHE_NAME = 'my-cache-v1';
+ const urlsToCache = [
+     '/',
+     'index.html',
+     'main.js',
+     'styles.css'
+ ];
+ 
+ // Install event: caching app shell
+ self.addEventListener('install', event => {
+     event.waitUntil(
+         caches.open(CACHE_NAME).then(cache => {
+             return cache.addAll(urlsToCache);
+         })
+     );
+ });
+ 
+ // Fetch event: serving app shell from cache
+ self.addEventListener('fetch', event => {
+     event.respondWith(
+         caches.match(event.request).then(response => {
+             return response || fetch(event.request);
+         })
+     );
+ });
+ 
+ // Activate event: updating the cache
+ self.addEventListener('activate', event => {
+     const cacheWhitelist = [CACHE_NAME];
+     event.waitUntil(
+         caches.keys().then(cacheNames => {
+             return Promise.all(
+                 cacheNames.map(cacheName => {
+                     if (cacheWhitelist.indexOf(cacheName) === -1) {
+                         return caches.delete(cacheName);
+                     }
+                 })
+             );
+         })
+     );
+ });
+ 
+ // Listening for the message event to trigger update
+ self.addEventListener('message', event => {
+     if (event.data === 'skipWaiting') {
+         self.skipWaiting();
+     }
+ });
